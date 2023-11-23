b86e3f3ab6d33500b739d48110f8e459b4ec6842
Jude Agboola <marvinjudehk@gmail.com>
2023-11-23 13:56:44 +0100

    cleanup

--- db/prisma/seed.ts
+++ db/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -11,29 +11,29 @@ const prisma = new PrismaClient();
 async function main() {
   const allStandardRecordTypes = [
     {
-      id: '653e31bc-6642-4f56-b120-1aca796ca66d',
-      name: 'Person',
-      description: 'Manage People',
-      slug: 'people',
+      id: "653e31bc-6642-4f56-b120-1aca796ca66d",
+      name: "Person",
+      description: "Manage People",
+      slug: "people",
     },
     {
-      id: '48f4fa99-435d-4375-a219-fa292c5f170b',
-      name: 'Deal',
-      description: 'Manage Deals',
-      slug: 'deals',
+      id: "48f4fa99-435d-4375-a219-fa292c5f170b",
+      name: "Deal",
+      description: "Manage Deals",
+      slug: "deals",
     },
     {
-      id: '4d8893d4-ef0a-4402-b544-be111790ac0f',
-      name: 'Company',
-      description: 'Manage Companies',
-      slug: 'companies',
+      id: "4d8893d4-ef0a-4402-b544-be111790ac0f",
+      name: "Company",
+      description: "Manage Companies",
+      slug: "companies",
     },
   ];
 
   for (const standardRecordType of allStandardRecordTypes) {
     const { id, name, description, slug } = standardRecordType;
 
-    await prisma.srts.upsert({
+    await prisma.sSrt.upsert({
       where: { id },
       update: {},
       create: {
@@ -49,7 +49,7 @@ main()
   .then(async () => {
     await prisma.$disconnect();
   })
-  .catch(async e => {
+  .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
